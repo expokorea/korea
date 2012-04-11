d08001cd24a6dd957797db4c8f3dfab37cf91681
arturoc <arturo@openframeworks.cc>
2012-04-11 12:17:45 +0200

    OscContourClient: bernsen threholding for height change detection

--- common/src/comm/OscContoursClient.cpp
+++ common/src/comm/OscContoursClient.cpp
@@ -14,8 +14,12 @@ OscContoursClient::OscContoursClient() {
 
 
 void OscContoursClient::setup(int port){
+	blobs.resize(2);
+	prevBlobs.resize(2);
 	osc.setup(port);
+#ifdef USE_ZEROCONF
 	avahi.start("oscservercontours","_oscit._udp",port);
+#endif
 }
 
 void OscContoursClient::update(){
@@ -23,15 +27,44 @@ void OscContoursClient::update(){
 	while(osc.hasWaitingMessages()){
 		osc.getNextMessage(&msg);
 		if(msg.getAddress()=="frame"){
-			blobs.clear();
+			int server=msg.getArgAsInt32(0);
+			prevBlobs[server] = blobs[server];
+			blobs[server].clear();
 			continue;
 		}
 
 		if(msg.getAddress()=="blob"){
+			int server=msg.getArgAsInt32(0);
+			blobs[server].push_back(Blob(server,msg.getArgAsInt32(1),
+				ofPoint(msg.getArgAsFloat(2), msg.getArgAsFloat(3)),
+				msg.getArgAsFloat(4)));
 
-			blobs.push_back(Blob(msg.getArgAsInt32(0),
-				ofPoint(msg.getArgAsFloat(1), msg.getArgAsFloat(2)),
-				msg.getArgAsFloat(3)));
+			for(int i=0;i<prevBlobs[server].size();i++){
+				if(blobs[server].back().id==prevBlobs[server][i].id){
+					blobs[server].back().sizeHist = prevBlobs[server][i].sizeHist;
+					break;
+				}
+			}
+			float max=-1, min=99;
+
+			deque<float>::iterator it;
+			for(it=blobs[server].back().sizeHist.begin();it!=blobs[server].back().sizeHist.end();it++){
+				if(max<*it) max=*it;
+				if(min>*it) min=*it;
+			}
+
+			blobs[server].back().sizeHist.push_back(blobs[server].back().size);
+			if(blobs[server].back().sizeHist.size()>runAwayFramesThreshold){
+				blobs[server].back().sizeHist.pop_front();
+			}
+
+			if(blobs[server].back().sizeHist.size()>5 && blobs[server].back().size-(max+min)*0.5 > (max+min)*0.5 * (1-runAwaySensitivity)){
+				blobs[server].back().suddenChange = true;
+			}else{ // if(!eyesOpened && avg-(max+min)*0.5 > .2){
+				blobs[server].back().suddenChange = false;
+			}
+
+			//cout <<msg.getArgAsFloat(1) << msg.getArgAsFloat(2) << endl;
 		}
 
 		/*if(msg.getAddress()=="contour"){
@@ -48,14 +81,10 @@ void OscContoursClient::update(){
 	}
 }
 
-vector<OscContoursClient::Blob> & OscContoursClient::getBlobs(){
-	return blobs;
+vector<OscContoursClient::Blob> & OscContoursClient::getBlobs(int server){
+	return blobs[server];
 }
 
-unsigned int OscContoursClient::size(){
-	return blobs.size();
-}
-
-OscContoursClient::Blob & OscContoursClient::operator[](int i){
-	return blobs[i];
+unsigned int OscContoursClient::size(int server){
+	return blobs[server].size();
 }
--- common/src/comm/OscContoursClient.h
+++ common/src/comm/OscContoursClient.h
@@ -9,7 +9,16 @@
 
 #include "ofConstants.h"
 #include "ofxOsc.h"
+#include "ofxGui.h"
+#include <deque>
+
+#ifdef TARGET_LINUX
+#define USE_ZEROCONF
+#endif
+
+#ifdef USE_ZEROCONF
 #include "ofxAvahiClient.h"
+#endif
 
 class OscContoursClient {
 public:
@@ -19,23 +28,32 @@ public:
 	void update();
 
 	struct Blob{
-		Blob(unsigned int id, const ofPoint & pos, float size)
-		:id(id)
+		Blob(int servernum, unsigned int id, const ofPoint & pos, float size)
+		:servernum(servernum)
+		,id(id)
 		,pos(pos)
 		,size(size){}
 
+		int servernum;
 		unsigned int id;
 		ofPoint pos;
 		float size;
+		deque<float> sizeHist;
+		bool suddenChange;
 	};
 
-	vector<Blob> & getBlobs();
-	unsigned int size();
-	Blob & operator[](int i);
+	vector<Blob> & getBlobs(int server);
+	unsigned int size(int server);
+
+	ofxParameter<float> runAwaySensitivity;
+	ofxParameter<int> runAwayFramesThreshold;
 
 private:
 	ofxOscReceiver osc;
+#ifdef USE_ZEROCONF
 	ofxAvahiClientService avahi;
-	vector<Blob> blobs;
+#endif
+	vector<vector<Blob> >blobs;
+	vector<vector<Blob> >prevBlobs;
 };
 
